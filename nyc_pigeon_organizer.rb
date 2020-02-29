d442e3f59ea7423b208c7a79583ce25e5501562d
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-29 22:16:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+def add_to_new_hash(header, subheader, h)
+
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  reorganized = {}
+  data.each do |header, subhash|
+    subhash.each do |subheader, names|
+      names.each do |name|
+        if !reorganized[name]
+          reorganized[name] = {}
+        end
+        if !reorganized[name][header]:
+          reorganized[name][header] = []
+        end
+        reorganized[name][header] = subheader.to_s
+      end
+    end
+  end
+  return reorganized
 end
